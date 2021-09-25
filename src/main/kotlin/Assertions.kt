bb368c5f2cf2dd8d498f15c904770db9ad531822
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-09-26 01:25:14 +0200

    doc

--- src/main/kotlin/Assertions.kt
+++ src/main/kotlin/Assertions.kt
@@ -5,11 +5,11 @@ import org.opentest4j.AssertionFailedError
 
 class UnexpectedIssue(val issueSpec: Specification.Issue, val source: String?) : AssertionFailedError("Unexpected $issueSpec at $source") {
     companion object {
-        fun test(testCase: Specification.TestCase, outcome: OperationOutcome): List<UnexpectedIssue> {
+        fun test(testSpec: Specification.TestCase, outcome: OperationOutcome): List<UnexpectedIssue> {
             val unexpectedErrorFailures = outcome.issue
                 .map { UnexpectedIssue(it.toData(), it.sourceUrl()) }
                 .filter { it.issueSpec.severity.failure() }
-                .filterNot { testCase.expectedIssues.any { expected -> expected.semanticallyEquals(it.issueSpec) } }
+                .filterNot { testSpec.expectedIssues.any { expected -> expected.semanticallyEquals(it.issueSpec) } }
 
             val color = if (unexpectedErrorFailures.isEmpty()) Color.SUCCESSFUL else Color.FAILED
             println(color.paint("  ${unexpectedErrorFailures.count()} unexpected error(s)!"))
@@ -21,16 +21,16 @@ class UnexpectedIssue(val issueSpec: Specification.Issue, val source: String?) :
 
 class MissingIssue(val issueSpec: Specification.Issue) : AssertionFailedError("Expected issue was not found: $issueSpec.") {
     companion object {
-        fun test(testCase: Specification.TestCase, outcome: OperationOutcome): List<MissingIssue> {
+        fun test(testSpec: Specification.TestCase, outcome: OperationOutcome): List<MissingIssue> {
             val issues = outcome.issue.map { it.toData() }
 
-            val missingIssueFailures = testCase.expectedIssues
+            val missingIssueFailures = testSpec.expectedIssues
                 .filterNot { expected -> issues.any { expected.semanticallyEquals(it) } }
                 .map { MissingIssue(it) }
 
-            val foundCount = testCase.expectedIssues.count() - missingIssueFailures.count()
-            val color = if (foundCount == testCase.expectedIssues.count()) Color.SUCCESSFUL else Color.FAILED
-            println(color.paint("  Found $foundCount of ${testCase.expectedIssues.count()} expected issue(s)!"))
+            val foundCount = testSpec.expectedIssues.count() - missingIssueFailures.count()
+            val color = if (foundCount == testSpec.expectedIssues.count()) Color.SUCCESSFUL else Color.FAILED
+            println(color.paint("  Found $foundCount of ${testSpec.expectedIssues.count()} expected issue(s)!"))
 
             return missingIssueFailures
         }
